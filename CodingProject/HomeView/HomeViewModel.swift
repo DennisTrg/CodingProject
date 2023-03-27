b8642f8d803366a9a07f2b1070a93d1ab3ccd0aa
Tung Truong <tungtruongthe@gmail.com>
2023-03-27 12:36:05 +0700

    [ADD] adding Home View Model

--- CodingProject/HomeView/HomeViewModel.swift
+++ CodingProject/HomeView/HomeViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+public enum DataError: Error {
+    case noData
+}
+final class HomeViewModel {
+    private let apiService: APIServiceProtocol
+    var listMovies: [Movie]?
+    var imageData: Data?
+    init(apiService: APIServiceProtocol = APIService()) {
+        self.apiService = apiService
+    }
+    
+    func fetchMovieResult(urlString: String) {
+        Task {
+            do {
+                listMovies = try await apiService.fetchApi(url: urlString)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    func fetchImage(imageString: String, completion: @escaping(Result<Data,DataError>) -> Void) {
+        
+        Task {
+            do {
+                imageData = try await apiService.getImage(imageString: imageString)
+                completion(.success(imageData!))
+            } catch {
+                completion(.failure(.noData))
+            }
+            
+        }
+    }
+}
